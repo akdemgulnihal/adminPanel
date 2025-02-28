9da7bf5043c3346f66797423d19d6566d6b95dd8
S4 <S4@DESKTOP-KVGSPIL>
2025-02-28 17:07:30 +0300

    Initial commit

---
+++ AdminPanel/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /****
+         *  Bu fonksiyonun amacı pencerenin taşınmasını sağlamak
+         * 
+         *  MouseButtonEventArgs e   fare ile ilgili bilgileri içeren bir nesnedir.
+         *  object sender            hangi öğede gerçekleştiğini belirtir. (bu durumda Border öğesi)
+         */
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Farenin sol tuşuna basıldığında - When the left mouse button was pressed
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                // Move the window when the left mouse button is pressed and dragged
+                this.DragMove();
+            }
+        }
+
+        private bool isMaximized = false;
+
+        /**
+         * 
+         * MouseButtonEventArgs e   fare ile ilgili bilgileri içeren bir nesnedir.
+         * object sender            hangi öğede gerçekleştiğini belirtir. (bu durumda Border öğesi)
+         */
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (isMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    isMaximized = false;
+
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    isMaximized = true;
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
