53dd76e8341983f994d90be3c108ecd551ff87c4
S4 <S4@DESKTOP-KVGSPIL>
2025-03-04 16:54:03 +0300

    Responsive olması için Width ve Height değerlerinde değişiklikler denendi.

--- AdminPanel/UserLoginPage.xaml.cs
+++ AdminPanel/UserLoginPage.xaml.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
 using System.Windows;
+using System.Windows.Data;
 using AdminPanel;
 
 namespace AdminPanel
 {
+    public class IsGreaterThanConverter : IValueConverter
+    {
+        public static IsGreaterThanConverter Instance { get; } = new IsGreaterThanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                if (parameter != null && double.TryParse(parameter.ToString(), out double threshold))
+                {
+                    return width > threshold;
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
     /// <summary>
     /// Interaction logic for UserLogin.xaml
     /// </summary>
@@ -63,5 +86,8 @@ namespace AdminPanel
             adminLoginPage.Show(); // Open the AdminLoginPage as a new window
             this.Close(); // Close the current login window
         }
+
     }
+
+
 }
